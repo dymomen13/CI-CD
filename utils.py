eec2e33a13e98f630fbecc4b0f036abc8a527333
dymomen13 <dymomen13@wp.pl>
2025-05-08 10:22:58 +0200

    inna

--- utils.py
+++ utils.py
@@ -1,11 +1,26 @@
+"""dodawanie"""
 
-def add ( a : int , b : int ) -> int :
+
+def add(a: int, b: int) -> int:
     return a + b
-def subtract (a : int , b : int ) -> int:
+
+
+"""odejmowanie"""
+
+
+def subtract(a: int, b: int) -> int:
     return a - b
-def multiply ( a : int , b : int ) -> int :
+
+
+"""mnozenie"""
+
+
+def multiply(a: int, b: int) -> int:
     return a * b
-def divide ( a : int , b : int ) -> float :
+
+
+"""dzielenie"""
+
+
+def divide(a: int, b: int) -> float:
     return a / b
-
-
