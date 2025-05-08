2a584b79a2735cf2c7e7ca8a35031a30f3182989
dymomen13 <dymomen13@wp.pl>
2025-05-08 10:26:33 +0200

    inna2

--- utils.py
+++ utils.py
@@ -1,26 +1,56 @@
-"""dodawanie"""
-
-
 def add(a: int, b: int) -> int:
+    """
+    Funkcja dodająca dwie liczby.
+
+    Args:
+        a (int): Pierwsza liczba.
+        b (int): Druga liczba.
+
+    Returns:
+        int: Suma dwóch liczb.
+    """
     return a + b
 
 
-"""odejmowanie"""
-
-
 def subtract(a: int, b: int) -> int:
+    """
+    Funkcja odejmująca dwie liczby.
+
+    Args:
+        a (int): Pierwsza liczba.
+        b (int): Druga liczba.
+
+    Returns:
+        int: Różnica dwóch liczb.
+    """
     return a - b
 
 
-"""mnozenie"""
-
-
 def multiply(a: int, b: int) -> int:
+    """
+    Funkcja mnożąca dwie liczby.
+
+    Args:
+        a (int): Pierwsza liczba.
+        b (int): Druga liczba.
+
+    Returns:
+        int: Iloczyn dwóch liczb.
+    """
     return a * b
 
 
-"""dzielenie"""
-
-
 def divide(a: int, b: int) -> float:
+    """
+    Funkcja dzieląca dwie liczby.
+
+    Args:
+        a (int): Pierwsza liczba.
+        b (int): Druga liczba.
+
+    Returns:
+        float: Wynik dzielenia.
+    """
+    if b == 0:
+        raise ValueError("Nie można dzielić przez zero.")
     return a / b
